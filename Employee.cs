ce6f59290a5e80823e10872dbc8fcc818b221909
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-12-04 09:49:41 +0100

    Creating employee class
    - creating employee class
    - setting employee properties.
    - created employee object constructor.
    - By overriding the ToString() method in your Employee class, you can define how an instance of the Employee class should be represented as a string. In this case, the overridden ToString() method returns a string that includes the ID, Name, Gender, and Salary of the employee formatted in a specific way.

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP_Generic_in_C_sharp
+{
+    public class Employee
+    {
+        // properties for employees
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+
+
+        // employee object constructor
+        public Employee(int id, string name, string gender, double salary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+
+        /*
+
+        By overriding the ToString() method in Employee class, it can define how
+        an instance of the Employee class should be represented as a string.
+        In this case, the overridden ToString() method returns a string that
+        includes the ID, Name, Gender, and Salary of the employee formatted in
+        a specific way.
+         
+        */
+        public override string ToString()
+        {
+            return $"ID = {Id}, Name = {Name}, Gender = {Gender}, Salary = {Salary}";
+        }
+    }
+
+}
